f61e109912af6d05841d888b44b8f0d3dae3d723
bala_b@hotmail.com <bala_b@hotmail.com>
2020-07-13 16:46:33 -0500

    upd

--- scripts-operationalize/agent-dependencies.sh
+++ scripts-operationalize/agent-dependencies.sh
@@ -2,4 +2,4 @@ python --version
 pip install azure-cli==2.0.72
 pip install --upgrade azureml-sdk
 pip install --upgrade azureml-sdk[cli]
-pip install tensorflow==2.2.0
+pip install tensorflow==1.13.0
