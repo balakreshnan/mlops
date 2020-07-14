5a7a9db51ed0de8eb95738dda9c63eed5babc69e
Bala <bala_b@hotmail.com>
2020-07-13 19:24:22 -0500

    update model

---
+++ tensorflow/notebooks/deploy/batchscore.py
@@ -0,0 +1,115 @@
+from azureml.core import Workspace
+ws = Workspace.from_config()
+
+from azureml.core.datastore import Datastore
+
+batchscore_blob = Datastore.register_azure_blob_container(ws, 
+                      datastore_name="images_datastore", 
+                      container_name="sampledata", 
+                      account_name="pipelinedata", 
+                      overwrite=True)
+
+def_data_store = ws.get_default_datastore()
+
+from azureml.core.dataset import Dataset
+from azureml.pipeline.core import PipelineData
+
+input_images = Dataset.File.from_files((batchscore_blob, "batchscoring/images/"))
+label_ds = Dataset.File.from_files((batchscore_blob, "batchscoring/labels/"))
+output_dir = PipelineData(name="scores", 
+                          datastore=def_data_store, 
+                          output_path_on_compute="batchscoring/results")
+
+input_images = input_images.register(workspace = ws, name = "input_images")
+label_ds = label_ds.register(workspace = ws, name = "label_ds")
+
+from azureml.core.model import Model
+
+model = Model(ws, 'tf-dnn-mnist')
+
+from azureml.core.compute import AmlCompute, ComputeTarget
+from azureml.exceptions import ComputeTargetException
+compute_name = "gpucluster1"
+
+# checks to see if compute target already exists in workspace, else create it
+try:
+    compute_target = ComputeTarget(workspace=ws, name=compute_name)
+except ComputeTargetException:
+    config = AmlCompute.provisioning_configuration(vm_size="STANDARD_NC6",
+                                                   vm_priority="lowpriority", 
+                                                   min_nodes=0, 
+                                                   max_nodes=1)
+
+    compute_target = ComputeTarget.create(workspace=ws, name=compute_name, provisioning_configuration=config)
+    compute_target.wait_for_completion(show_output=True, min_node_count=None, timeout_in_minutes=20)
+
+from azureml.core import Environment
+from azureml.core.conda_dependencies import CondaDependencies
+from azureml.core.runconfig import DEFAULT_GPU_IMAGE
+
+cd = CondaDependencies.create(pip_packages=["tensorflow-gpu==1.15.2",
+                                            "azureml-core", "azureml-dataprep[fuse]"])
+env = Environment(name="parallelenv")
+env.python.conda_dependencies = cd
+env.docker.base_image = DEFAULT_GPU_IMAGE
+
+
+from azureml.pipeline.steps import ParallelRunConfig
+
+parallel_run_config = ParallelRunConfig(
+    environment=env,
+    entry_script="batch_scoring.py",
+    source_directory="scripts",
+    output_action="append_row",
+    mini_batch_size="20",
+    error_threshold=1,
+    compute_target=compute_target,
+    process_count_per_node=2,
+    node_count=1
+)
+
+from azureml.pipeline.steps import ParallelRunStep
+from datetime import datetime
+
+parallel_step_name = "batchscoring-" + datetime.now().strftime("%Y%m%d%H%M")
+
+label_config = label_ds.as_named_input("labels_input")
+
+batch_score_step = ParallelRunStep(
+    name=parallel_step_name,
+    inputs=[input_images.as_named_input("input_images")],
+    output=output_dir,
+    arguments=["--model_name", "inception",
+               "--labels_dir", label_config],
+    side_inputs=[label_config],
+    parallel_run_config=parallel_run_config,
+    allow_reuse=False
+)
+
+from azureml.core import Experiment
+from azureml.pipeline.core import Pipeline
+
+pipeline = Pipeline(workspace=ws, steps=[batch_score_step])
+pipeline_run = Experiment(ws, 'batch_scoring').submit(pipeline)
+pipeline_run.wait_for_completion(show_output=True)
+
+import pandas as pd
+
+batch_run = next(pipeline_run.get_children())
+batch_output = batch_run.get_output_data("scores")
+batch_output.download(local_path="inception_results")
+
+for root, dirs, files in os.walk("inception_results"):
+    for file in files:
+        if file.endswith("parallel_run_step.txt"):
+            result_file = os.path.join(root, file)
+
+df = pd.read_csv(result_file, delimiter=":", header=None)
+df.columns = ["Filename", "Prediction"]
+print("Prediction has ", df.shape[0], " rows")
+df.head(10)
+
+published_pipeline = pipeline_run.publish_pipeline(
+    name="Inception_v3_scoring", description="Batch scoring using Inception v3 model", version="1.0")
+
+published_pipeline
